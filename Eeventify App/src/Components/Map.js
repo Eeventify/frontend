fc4925b7575d52f174dfad66559d3327ec1ed2d5
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-06-23 23:12:10 +0200

    Fixed marker anchor

--- Eeventify App/src/Components/Map.js
+++ Eeventify App/src/Components/Map.js
@@ -7,7 +7,9 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const DefaultIcon = L.icon({
     iconUrl: icon,
-    shadowUrl: iconShadow
+    shadowUrl: iconShadow,
+    iconSize: [25,41],
+    iconAnchor: [12,41]
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
